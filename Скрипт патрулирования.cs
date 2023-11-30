3a57499baadd8b9d74c8a4ad243c2ffea9179572
za4emmne <iluhaha@mail.ru>
2023-11-30 23:58:34 +0500

    Update MoveTowards 1.01

--- Скрипт патрулирования.cs
+++ Скрипт патрулирования.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class MoveTowards : MonoBehaviour
 {
     //1. В инспекторе необходмо создать пустые объекты, которые будут являться точками траектории и поместить их в ПУТЬ ТРАЕКТОРИИ
 
